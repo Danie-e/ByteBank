9b1e07d5afa95a95d2a9c5524ec3955b1741c888
Daniela Eloisa <danielaeloisa1@gmail.com>
2025-05-27 10:06:10 -0300

    Subindo arquivos iniciais para o projeto oferecidos pela Alura.

---
+++ ByteBank/Models/ADM/Utilitario/GerenciadorDeBonificacao.cs
@@ -0,0 +1,21 @@
+﻿
+using ByteBank.Modelos.ADM.Funcionarios;
+
+namespace ByteBank.Modelos.ADMs.Utilitario
+{
+    public class GerenciadorDeBonificacao
+    {
+        private double _totalBonificacao;
+
+        public void Registrar(Funcionario funcionario)
+        {
+            _totalBonificacao += funcionario.getBonificacao();
+        }
+
+        public double GetTotalBonificacao()
+        {
+            return _totalBonificacao;
+        }
+
+    }
+}
