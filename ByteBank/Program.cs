cfa272d72d9ca2f89091554453dd384fe31d78e4
Daniela Eloisa <danielaeloisa1@gmail.com>
2025-05-29 08:44:43 -0300

    Criando Array de Contas Correntes.

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ByteBank.Modelos.Conta;
+using ByteBank.Util;
+
 Console.WriteLine("Hello, World!");
+TestaArrayDeContas();
+
+void TestaArrayDeContas()
+{
+    ListaDeContasCorrentes listaDeContas = new ListaDeContasCorrentes();
+    listaDeContas.Adicionar(new ContaCorrente(001));
+    listaDeContas.Adicionar(new ContaCorrente(002));
+    listaDeContas.Adicionar(new ContaCorrente(003));
+    listaDeContas.Adicionar(new ContaCorrente(004));
+    listaDeContas.Adicionar(new ContaCorrente(005));
+    
+    
+}
---
+++ ByteBank/Util/ListaDeContasCorrentes.cs
@@ -0,0 +1,21 @@
+﻿using ByteBank.Modelos.Conta;
+
+namespace ByteBank.Util;
+
+public class ListaDeContasCorrentes
+{
+    public ListaDeContasCorrentes(int tamanhoInicial = 5)
+    {
+        _itens = new ContaCorrente[tamanhoInicial];
+    }
+
+    private ContaCorrente[] _itens = null;
+    private int _proximaPosicao = 0;
+
+    public void Adicionar(ContaCorrente conta)
+    {
+        Console.WriteLine($"Conta adicionado na posição {_proximaPosicao}");
+        _itens[_proximaPosicao] = conta;
+        _proximaPosicao++;
+    }
+}
