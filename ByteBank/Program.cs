721a7dc80f953ff6a09e76054670f5e636764e19
Daniela Eloisa <danielaeloisa1@gmail.com>
2025-06-05 08:50:28 -0300

    Utilizando TList e objeto genérico.

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,6 +1,5 @@
 ﻿using ByteBank.Modelos.Conta;
 using ByteBank.Util;
-using System.Collections;
 
 Console.WriteLine("Boas Vindas ao ByteBank Atendimento.!");
 //TestaArrayDeContas();
@@ -34,8 +33,13 @@ void TestaArrayDeContas()
 
 #endregion
 
-ArrayList _listaDeContas = new ArrayList();
-AtendimentoCliente();
+List<ContaCorrente> _listaDeContas = new List<ContaCorrente>()
+{
+    new ContaCorrente(95) {Saldo=100,Titular=new(){Nome="Daniela",Profissao="Dev",Cpf="123456789" } },
+    new ContaCorrente(95) {Saldo=200,Titular=new(){Nome="Julio",Profissao="Tester",Cpf="789456123" }},
+    new ContaCorrente(94) {Saldo=60,Titular=new(){Nome="Isabela",Profissao="Desiner",Cpf="741258963" }}
+};
+//AtendimentoCliente();
 void AtendimentoCliente()
 {
     char opcao = '0';
@@ -70,63 +74,77 @@ void AtendimentoCliente()
         }
 
     }
+}
 
-    void CadastrarConta()
+void CadastrarConta()
+{
+    Console.Clear();
+    Console.WriteLine("===============================");
+    Console.WriteLine("===   CADASTRO DE CONTAS    ===");
+    Console.WriteLine("===============================");
+    Console.WriteLine("\n");
+    Console.WriteLine("=== Informe dados da conta ===");
+
+    Console.Write("Número da Agência: ");
+    int numeroAgencia = int.Parse(Console.ReadLine());
+
+    ContaCorrente conta = new ContaCorrente(numeroAgencia);
+
+    Console.Write("Informe o saldo inicial: ");
+    conta.Saldo = double.Parse(Console.ReadLine());
+
+    Console.Write("Infome nome do Titular: ");
+    conta.Titular.Nome = Console.ReadLine();
+
+    Console.Write("Infome CPF do Titular: ");
+    conta.Titular.Cpf = Console.ReadLine();
+
+    Console.Write("Infome Profissão do Titular: ");
+    conta.Titular.Profissao = Console.ReadLine();
+
+    _listaDeContas.Add(conta);
+    Console.WriteLine("... Conta cadastrada com sucesso! ...");
+    Console.ReadKey();
+}
+
+void ListarContas()
+{
+    Console.Clear();
+    Console.WriteLine("===============================");
+    Console.WriteLine("===     LISTA DE CONTAS     ===");
+    Console.WriteLine("===============================");
+    Console.WriteLine("\n");
+
+    if (_listaDeContas.Count <= 0)
     {
-        Console.Clear();
-        Console.WriteLine("===============================");
-        Console.WriteLine("===   CADASTRO DE CONTAS    ===");
-        Console.WriteLine("===============================");
-        Console.WriteLine("\n");
-        Console.WriteLine("=== Informe dados da conta ===");
-
-        Console.Write("Número da Agência: ");
-        int numeroAgencia = int.Parse(Console.ReadLine());
-
-        ContaCorrente conta = new ContaCorrente(numeroAgencia);
-
-        Console.Write("Informe o saldo inicial: ");
-        conta.Saldo = double.Parse(Console.ReadLine());
-
-        Console.Write("Infome nome do Titular: ");
-        conta.Titular.Nome = Console.ReadLine();
-
-        Console.Write("Infome CPF do Titular: ");
-        conta.Titular.Cpf = Console.ReadLine();
-
-        Console.Write("Infome Profissão do Titular: ");
-        conta.Titular.Profissao = Console.ReadLine();
-
-        _listaDeContas.Add(conta);
-        Console.WriteLine("... Conta cadastrada com sucesso! ...");
+        Console.WriteLine("... Não há contas cadastradas! ...");
         Console.ReadKey();
+        return;
     }
 
-    void ListarContas()
+    foreach (ContaCorrente item in _listaDeContas)
     {
-        Console.Clear();
-        Console.WriteLine("===============================");
-        Console.WriteLine("===     LISTA DE CONTAS     ===");
-        Console.WriteLine("===============================");
-        Console.WriteLine("\n");
+        Console.WriteLine("===  Dados da Conta  ===");
+        Console.WriteLine("Número da Conta : " + item.Conta);
+        Console.WriteLine("Saldo da Conta : " + item.Saldo);
+        Console.WriteLine("Titular da Conta: " + item.Titular.Nome);
+        Console.WriteLine("CPF do Titular  : " + item.Titular.Cpf);
+        Console.WriteLine("Profissão do Titular: " + item.Titular.Profissao);
+        Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        Console.ReadKey();
+    }
+}
 
-        if (_listaDeContas.Count <= 0)
-        {
-            Console.WriteLine("... Não há contas cadastradas! ...");
-            Console.ReadKey();
-            return;
-        }
 
-        foreach (ContaCorrente item in _listaDeContas)
-        {
-            Console.WriteLine("===  Dados da Conta  ===");
-            Console.WriteLine("Número da Conta : " + item.Conta);
-            Console.WriteLine("Saldo da Conta : " + item.Saldo);
-            Console.WriteLine("Titular da Conta: " + item.Titular.Nome);
-            Console.WriteLine("CPF do Titular  : " + item.Titular.Cpf);
-            Console.WriteLine("Profissão do Titular: " + item.Titular.Profissao);
-            Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-            Console.ReadKey();
-        }
+Generica<int> teste1 = new Generica<int>();
+teste1.MostrarMensagem(0);
+
+Generica<string> teste2 = new Generica<string>();
+teste2.MostrarMensagem("teste");
+public class Generica<T>
+{
+    public void MostrarMensagem(T t)
+    {
+        Console.WriteLine($"Exibindo {t}");
     }
 }
