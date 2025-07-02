fe7b1a2e2abbc69c84239291c5ccc88cf1271db0
Danie-e <danielaeloisa1@gmail.com>
2025-07-02 09:53:15 -0300

    Criando laço de repetição para realizar a leitura de todos os bytes.

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -10,15 +10,29 @@ internal class Program
         string enderecoArquivo = "contas.txt";
         FileStream fluxoArquivo = new(enderecoArquivo, FileMode.Open);
 
+        int numeroDeBytesLidos = -1;
         var buffer = new byte[1024]; //1kb
-        fluxoArquivo.Read(buffer, 0, buffer.Length);
 
-        List<ContaCorrente> _listaDeContas = new List<ContaCorrente>()
-{
-    new ContaCorrente(95) {Saldo=100,Titular=new(){Nome="Daniela",Profissao="Dev",Cpf="123456789" } },
-    new ContaCorrente(95) {Saldo=200,Titular=new(){Nome="Julio",Profissao="Tester",Cpf="789456123" }},
-    new ContaCorrente(94) {Saldo=60,Titular=new(){Nome="Isabela",Profissao="Desiner",Cpf="741258963" }}
-};
-        new Atendimento().AtendimentoCliente();
+        while (numeroDeBytesLidos != 0)
+        {
+            numeroDeBytesLidos = fluxoArquivo.Read(buffer, 0, buffer.Length);
+            EscreverBuffer(buffer);
+        }
+
+        //        List<ContaCorrente> _listaDeContas = new List<ContaCorrente>()
+        //{
+        //    new ContaCorrente(95) {Saldo=100,Titular=new(){Nome="Daniela",Profissao="Dev",Cpf="123456789" } },
+        //    new ContaCorrente(95) {Saldo=200,Titular=new(){Nome="Julio",Profissao="Tester",Cpf="789456123" }},
+        //    new ContaCorrente(94) {Saldo=60,Titular=new(){Nome="Isabela",Profissao="Desiner",Cpf="741258963" }}
+        //};
+        //        new Atendimento().AtendimentoCliente();
+    }
+
+    static public void EscreverBuffer(byte[] buffer)
+    {
+        foreach (byte item in buffer)
+        {
+            Console.Write(item);
+        }
     }
 }
