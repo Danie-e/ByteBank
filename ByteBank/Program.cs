9486b67a04fa1900f14ce896871d4fcf2116f6d9
Danie-e <danielaeloisa1@gmail.com>
2025-07-02 09:14:26 -0300

    Apagando código que não está sendo utilizado e criando fluxo para leitura do arquivo txt.

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,98 +1,24 @@
 ﻿using ByteBank.Atendimento;
-using ByteBank.Exceptions;
 using ByteBank.Modelos.Conta;
-using ByteBank.Util;
 
-Console.WriteLine("Boas Vindas ao ByteBank Atendimento.!");
-//TestaArrayDeContas();
-
-#region Exemplo Array Contas
-
-void TestaArrayDeContas()
+internal class Program
 {
-    ListaDeContasCorrentes listaDeContas = new ListaDeContasCorrentes();
-    ContaCorrente conta = new ContaCorrente(005);
-
-    listaDeContas.Adicionar(new ContaCorrente(001));
-    listaDeContas.Adicionar(new ContaCorrente(002));
-    listaDeContas.Adicionar(new ContaCorrente(003));
-    listaDeContas.Adicionar(new ContaCorrente(004));
-    listaDeContas.Adicionar(conta);
-    listaDeContas.Adicionar(new ContaCorrente(006));
-    //listaDeContas.ExibirLista();
-
-    listaDeContas.Remover(conta);
-    Console.WriteLine("");
-    //listaDeContas.ExibirLista();
-
-    for (int i = 0; i < listaDeContas.Tamanho; i++)
+    private static void Main(string[] args)
     {
-        ContaCorrente conta1 = listaDeContas[i];
-        Console.WriteLine($"Indice: {i}, Conta: {conta1.Numero_agencia}");
-    }
+        Console.WriteLine("Boas Vindas ao ByteBank Atendimento.!");
 
-}
+        string enderecoArquivo = "contas.txt";
+        FileStream fluxoArquivo = new(enderecoArquivo, FileMode.Open);
 
-#endregion
+        var buffer = new byte[1024]; //1kb
+        fluxoArquivo.Read(buffer, 0, buffer.Length);
 
-List<ContaCorrente> _listaDeContas = new List<ContaCorrente>()
+        List<ContaCorrente> _listaDeContas = new List<ContaCorrente>()
 {
     new ContaCorrente(95) {Saldo=100,Titular=new(){Nome="Daniela",Profissao="Dev",Cpf="123456789" } },
     new ContaCorrente(95) {Saldo=200,Titular=new(){Nome="Julio",Profissao="Tester",Cpf="789456123" }},
     new ContaCorrente(94) {Saldo=60,Titular=new(){Nome="Isabela",Profissao="Desiner",Cpf="741258963" }}
 };
-Atendimento atendimento = new();
-atendimento.AtendimentoCliente();
-
-#region Teste Listas
-
-//List<ContaCorrente> _listaDeContas2 = new List<ContaCorrente>()
-//{
-//    new ContaCorrente(874),
-//    new ContaCorrente(874),
-//    new ContaCorrente(874)
-//};
-
-//List<ContaCorrente> _listaDeContas3 = new List<ContaCorrente>()
-//{
-//    new ContaCorrente(951),
-//    new ContaCorrente(321),
-//    new ContaCorrente(719)
-//};
-
-//_listaDeContas2.AddRange(_listaDeContas3);
-//_listaDeContas2.Reverse();
-
-//for (int i = 0; i < _listaDeContas2.Count; i++)
-//{
-//    Console.WriteLine($"Indice[{i}] = Conta [{_listaDeContas2[i].Conta}]");
-//}
-
-//var range = _listaDeContas3.GetRange(0, 1);
-//for (int i = 0; i < range.Count; i++)
-//{
-//    Console.WriteLine($"Indice[{i}] = Conta [{range[i].Conta}]");
-//}
-
-//Console.WriteLine();
-//_listaDeContas3.Clear();
-//for (int i = 0; i < _listaDeContas3.Count; i++)
-//{
-//    Console.WriteLine($"Indice[{i}] = Conta [{_listaDeContas3[i].Conta}]");
-//}
-
-#endregion
-
-Generica<int> teste1 = new Generica<int>();
-//teste1.MostrarMensagem(0);
-
-Generica<string> teste2 = new Generica<string>();
-//teste2.MostrarMensagem("teste");
-public class Generica<T>
-{
-    public void MostrarMensagem(T t)
-    {
-        Console.WriteLine($"Exibindo {t}");
+        new Atendimento().AtendimentoCliente();
     }
 }
-
