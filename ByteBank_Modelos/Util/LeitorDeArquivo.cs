0f67ab6e869d6706641dfab376115fb9e6aad24f
Danie-e <danielaeloisa1@gmail.com>
2025-07-22 10:19:31 -0300

    Criando uma biblioteca de classes para conter os modelos utilizados na solução e inserindo dependência no projeto BayteBank  para que ele consiga utilizar as classes.

---
+++ ByteBank_Modelos/Util/LeitorDeArquivo.cs
@@ -0,0 +1,78 @@
+﻿using ByteBank.Modelos.Conta;
+using System.Text;
+
+namespace ByteBank.Util;
+class LeitorDeArquivo
+{
+    static public void LerArquivoTexto(string enderecoArquivo)
+    {
+        using (FileStream fluxoArquivo = new(enderecoArquivo, FileMode.Open))
+        {
+            int numeroDeBytesLidos = -1;
+            var buffer = new byte[1024]; //1kb
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoArquivo.Read(buffer, 0, buffer.Length);
+                EscreverBuffer(buffer);
+            }
+        }
+    }
+
+    static public void EscreverBuffer(byte[] buffer)
+    {
+        var utf8 = new UTF8Encoding();
+        string texto = utf8.GetString(buffer);
+        Console.Write(texto);
+    }
+
+    static ContaCorrente ConverteStringParaContaCorrente(string linha)
+    {
+        string[] campos = linha.Split(',');
+        ContaCorrente conta = new(int.Parse(campos[0]))
+        {
+            Saldo = double.Parse(campos[2].Replace('.', ',')),
+            Titular = new() { Nome = campos[3] }
+        };
+        return conta;
+    }
+
+    static void LeituraBinaria()
+    {
+        using (var fs = new FileStream("contaCorrente.txt", FileMode.Open))
+        using (var leitor = new BinaryReader(fs))
+        {
+            Int32 agencia = leitor.ReadInt32();
+            Int32 numeroDaConta = leitor.ReadInt32();
+            double saldo = leitor.ReadDouble();
+            string titular = leitor.ReadString();
+
+            Console.WriteLine($@"Agencia: {agencia}
+Numero da conta: {numeroDaConta}
+Saldo: {saldo}
+Titular: {titular}");
+        }
+    }
+
+    static void UsarStreamDeEntrada()
+    {
+        {
+            using (var fluxoDeEntrada = Console.OpenStandardInput())
+            using (var fs = new FileStream("entradaConsole.txt", FileMode.Create))
+            {
+                var buffer = new byte[1024]; //1kb
+
+                while (true)
+                {
+                    var byteslidos = fluxoDeEntrada.Read(buffer, 0, 1024);
+
+                    fs.Write(buffer, 0, byteslidos);
+                    fs.Flush();
+
+                    Console.WriteLine($"Bytes lidos na console: {byteslidos}");
+                }
+            }
+        }
+    }
+
+}
---
+++ ByteBank_Modelos/Util/ListaDeContasCorrentes.cs
@@ -0,0 +1,89 @@
+﻿using ByteBank.Modelos.Conta;
+
+namespace ByteBank.Util;
+
+public class ListaDeContasCorrentes
+{
+    public ListaDeContasCorrentes(int tamanhoInicial = 4)
+    {
+        _itens = new ContaCorrente[tamanhoInicial];
+    }
+
+    public ContaCorrente this[int indice]
+    {
+        get { return RecuperarContaIndice(indice); }
+    }
+
+    private ContaCorrente[] _itens = null;
+    private int _proximaPosicao = 0;
+    public int Tamanho
+    {
+        get
+        {
+            return _proximaPosicao;
+        }
+    }
+    public void Adicionar(ContaCorrente conta)
+    {
+        //Console.WriteLine($"Conta adicionado na posição {_proximaPosicao}");
+        VerificarCapacidade(_proximaPosicao + 1);
+        _itens[_proximaPosicao] = conta;
+        _proximaPosicao++;
+    }
+
+    public void Remover(ContaCorrente conta)
+    {
+        int indiceItem = 1;
+        for (int i = 0; i < _proximaPosicao; i++)
+        {
+            ContaCorrente contaAtual = _itens[i];
+            if (contaAtual == conta)
+            {
+                indiceItem = i;
+                break;
+            }
+        }
+
+        for (int i = indiceItem; i < _proximaPosicao - 1; i++)
+        {
+            _itens[i] = _itens[i + 1];
+        }
+        _proximaPosicao--;
+        _itens[_proximaPosicao] = null;
+    }
+
+    public void ExibirLista()
+    {
+        for (int i = 0; i < _itens.Length; i++)
+        {
+            if (_itens[i] != null)
+            {
+                Console.WriteLine($"Conta: {_itens[i].Conta}");
+            }
+        }
+    }
+
+    public ContaCorrente RecuperarContaIndice(int indice)
+    {
+        if (indice < 0 || indice >= _proximaPosicao)
+        {
+            throw new ArgumentOutOfRangeException(nameof(indice));
+        }
+        return _itens[indice];
+    }
+
+    private void VerificarCapacidade(int tamanhoNecessario)
+    {
+        if (_itens.Length >= tamanhoNecessario)
+            return;
+        //Console.WriteLine("Aumentando capacidade do array");
+        ContaCorrente[] novoArray = new ContaCorrente[tamanhoNecessario];
+
+        for (int i = 0; i < _itens.Length; i++)
+        {
+            novoArray[i] = _itens[i];
+        }
+
+        _itens = novoArray;
+    }
+}
