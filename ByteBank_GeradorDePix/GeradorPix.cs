27865e51b20049809dae2ab9f52d01211175c65e
Danie-e <danielaeloisa1@gmail.com>
2025-08-04 09:03:11 -0300

    Criando documentação para exibir informações relevantes sobre as classes e métodos.

--- ByteBank_GeradorDePix/GeradorPix.cs
+++ ByteBank_GeradorDePix/GeradorPix.cs
@@ -1,12 +1,25 @@
 ﻿namespace ByteBank_GeradorDePix
 {
+    /// <summary>
+    /// Classe para gerar chaves pix usando o formato Guid. 
+    /// </summary>
+    /// <returns></returns>
     public static class GeradorPix
     {
+        /// <summary>
+        /// Método para gerar uma chave aleatória pix usando o formato Guid. 
+        /// </summary>
+        /// <returns>Retorna uma chave pux no formato string</returns>
         public static string GetChavePix()
         {
             return Guid.NewGuid().ToString();
         }
 
+        /// <summary>
+        /// Método para gerar chaves pix de acordo com a quantidade passada no parâmetro usando o formato Guid. 
+        /// </summary>
+        /// <param name="numeroDeChaves">Quantidade de chaves a serem geradas</param>
+        /// <returns>Lista de chaves no formato string</returns>
         public static List<string> GetChavesPix(int numeroDeChaves)
         {
             if (numeroDeChaves <= 0)
